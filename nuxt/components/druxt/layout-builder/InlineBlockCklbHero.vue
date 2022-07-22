4296fe131273ddff02165154fa5f3a5b19fd1292
Stuart Clark <stu@rtclark.net>
2022-07-22 02:42:41 +0000

    chore: update background styles

--- nuxt/components/druxt/layout-builder/InlineBlockCklbHero.vue
+++ nuxt/components/druxt/layout-builder/InlineBlockCklbHero.vue
@@ -1,6 +1,5 @@
 <template>
-  <div v-if="!$fetchState.pending">
-    <img :src="file.attributes.uri.url" />
+  <div v-if="!$fetchState.pending" :style="containerStyles">
     <DruxtDebug :json="{ component, entity }" />
   </div>
 </template>
@@ -21,27 +20,46 @@ export default {
   },
 
   data: () => ({
+    block: undefined,
     file: undefined,
     media: undefined,
   }),
 
   async fetch() {
+    // @TODO - Fetch the block (block_content--cklb_hero)
+
     if (this.hasBackgroundImage) {
-      const { data, included } = await this.$store.dispatch('druxt/getCollection', {
+      const backgroundImage = await this.$store.dispatch('druxt/getCollection', {
         type: 'media--image',
         query: new DrupalJsonApiParams()
           .addFilter('drupal_internal__mid', this.backgroundImageId)
           .addInclude('field_media_image')
       })
-      this.media = data[0]
-      this.file = included.find((o) => o.id === this.media.relationships.field_media_image.data.id)
+      this.media = backgroundImage.data[0]
+      this.file = backgroundImage.included.find((o) => o.id === this.media.relationships.field_media_image.data.id)
     }
   },
 
   computed: {
+    blockStyle: ({ component }) => component.additional.bootstrap_styles.block_style,
+
     // Background image:
-    backgroundImageId: ({ component }) => component.additional.bootstrap_styles.block_style.background_media.image.media_id,
-    hasBackgroundImage: ({ backgroundImageId }) => !!backgroundImageId,
+    backgroundImageId: ({ blockStyle }) => blockStyle.background_media.image.media_id,
+    hasBackgroundImage: ({ blockStyle, backgroundImageId }) => blockStyle.background.background_type === 'image' && !!backgroundImageId,
+
+    containerStyles: ({ blockStyle, hasBackgroundImage, file }) => {
+      const styles = {}
+      if (hasBackgroundImage && file) {
+        const options = blockStyle.background_media.background_options
+        styles.backgroundImage = `url(${file.attributes.uri.url})`
+        styles.backgroundPosition = options.background_position
+        styles.backgroundRepeat = options.background_repeat
+        styles.backgroundSize = options.background_size
+        if (options.background_attachement === "fixed") styles.backgroundAttachment = 'fixed'
+      }
+
+      return styles
+    }
   }
 }
 </script>
